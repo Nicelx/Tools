3ab979cf01ad63e0ef78e452c4821c76f9b49aa8
Nicelx <relush321@gmail.com>
2025-02-05 17:52:05 +0300

    remove files

--- dynamic-injection/index.js
+++
@@ -1,7 +0,0 @@
-function n__inject(fromSelector, toSelector) {
-    // v. 1.01
-    const fromEl = document.querySelector(fromSelector);
-    const toEl = document.querySelector(toSelector);
-
-    toEl.innerHTML = fromEl.innerHTML;
-}
