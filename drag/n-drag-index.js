38b9341531c63d98fac4e18d0b74e7b00c2e6641
Nicelx <relush321@gmail.com>
2025-02-10 13:22:13 +0300

    drag component: add disable method, add init method, rewrite handlers

--- drag/n-drag-index.js
+++ drag/n-drag-index.js
@@ -1,23 +1,39 @@
+//  TO DO:
+// 
+/*
+    1) elastic borders. 
+    2) multiple tracks, one instance??? to think
+*/
 class NDrag {
-    // v.0.1 test period
+    // v.0.2 test 
     constructor(selector, options) {
         this.track = document.querySelector(selector + ' .n-drag__track');
-        this.track.addEventListener('mousedown', e => { this.mouseDownHandler(e) });
-        document.addEventListener('mousemove', e => { this.mouseMoveHandler(e) });
-        document.addEventListener('mouseup', e => {this.mouseUpHandler(e);})
-        console.log(this.track);
+        this.init();
     }
-    isDragging = false;
-    startingX = 0;
-    initialPosition = 0;
 
-    mouseDownHandler(e) {
+    init() {
+        this.isDragging = false;
+        this.startingX = 0;
+        this.initialPosition = 0;
+
+        this.track.addEventListener('mousedown', this.mouseDownHandler);
+        document.addEventListener('mousemove', this.mouseMoveHandler);
+        document.addEventListener('mouseup', this.mouseUpHandler)
+    }
+    disable() {
+        this.track.removeEventListener('mousedown', this.mouseDownHandler)
+        document.removeEventListener('mousemove', this.mouseMoveHandler);
+        document.removeEventListener('mouseup', this.mouseUpHandler)
+        this.track.style.transform = `translateX(0px)`;
+    }
+   
+
+    mouseDownHandler = (e) => {
         this.isDragging = true;
-        // console.log(e.clientX);
-        console.log('offset left', this.track.offsetLeft);
         this.startingX = e.clientX;
     }
-    mouseMoveHandler(e) {
+    
+    mouseMoveHandler = (e) => {
         if (!this.isDragging) return;
         let delta;
         if (this.startingX == 0) {
@@ -25,22 +41,15 @@ class NDrag {
         } else {
             delta = e.clientX - this.startingX;
         }
-        // let delta = this.startingX - e.clientX;
 
         this.track.style.transform = `translateX(${this.initialPosition + delta}px)`;
     }
 
-    mouseUpHandler(e) {
+    mouseUpHandler = (e) => {
         if (!this.isDragging) return;
         this.initialPosition = this.initialPosition + e.clientX - this.startingX;
         this.track.style.transform = `translateX(${this.initialPosition})`;
         this.startingX = 0;
         this.isDragging = false;
-    }
-
-    clear() {
-        this.track.addEventListener('mousedown', e => { this.mouseDownHandler(e) });
-        document.addEventListener('mousemove', e => { this.mouseMoveHandler(e) });
-        document.addEventListener('mouseup', e => {this.mouseUpHandler(e);})
     }
 }
