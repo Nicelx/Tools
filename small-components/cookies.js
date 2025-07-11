d5aa0673cae55c3d3df167d8f551757cd115154b
Nicelx <relush321@gmail.com>
2025-07-11 13:01:05 +0300

    edit cookies.js, fix width, add accept btn

--- small-components/cookies.js
+++ small-components/cookies.js
@@ -1,47 +1,46 @@
 (function () {
-    const accept = localStorage.getItem('cookies_accept')
-    
-    if (accept) return;
+  const accept = localStorage.getItem("cookies_accept");
 
-    const string = `
+  if (accept) return;
+
+  const string = `
     <div class="n-cookies">
         <style>
             .n-cookies {
                 position: fixed;
                 bottom: 0;
                 left: 0;
-                width: 100%;
-                padding: 8px 12px;
+                padding: 24px;
                 background-color: black;
                 color: white;
+                text-align: center;
             }
-            .n-cookies__close {
-                position: absolute;
-                font-size: 20px;
-                top: 10px;
-                right: 40px;
-                cursor: pointer;
+
+            .n-cookies__link, a.n-cookies__link:visited, a.n-cookies__link:hover {
+                margin: 8px;
+                padding: 8px;
+                background-color: white;
+                color: var(--color-primary);
+                display: inline-block;
             }
+            
         </style>
-        <p>Наш сайт использует файлы куки для повышения качества предоставляемых услуг, аналитики, а также для настройки
-            удобства пользователей. Продолжая использование сайта, вы автоматически соглашаетесь с использованием файлов
-            cookie, что позволит нам улучшать наш сервис и предоставлять персонализированный опыт взаимодействия. Если
-            вы не согласны с использованием файлов куки, пожалуйста, измените настройки браузера или прекратите
-            использование сайта.</p>
-        <div class = "n-cookies__close">
-            &times;
+        <p>
+            Мы используем файлы «Cookie» для повышения качества предоставляемых услуг, аналитики, а также для настройки удобства пользователей.<br>
+Нажимая кнопку «Принять» и продолжая пользоваться сайтом, вы соглашаетесь на обработку файлов «Cookie». Если вы не согласны с использованием файлов «Cookie», пожалуйста, измените настройки браузера или прекратите использование сайта.
+        </p>
+        <div style = "margin-top: 20px">
+            <a class = "n-cookies__link">Принять</a>
         </div>
     </div>
 `;
-    document.body.insertAdjacentHTML('beforeend', string);
 
-    const nCookies = document.querySelector('.n-cookies');
-    const close = document.querySelector('.n-cookies__close');
+  document.body.insertAdjacentHTML("beforeend", string);
+  const nCookies = document.querySelector(".n-cookies");
+  const close = document.querySelector(".n-cookies__link");
 
-    close.addEventListener('click', () => {
-        nCookies.style.display = "none";
-        localStorage.setItem('cookies_accept', '1');
-    });
-})()
-
-
+  close.addEventListener("click", () => {
+    nCookies.style.display = "none";
+    localStorage.setItem("cookies_accept", "1");
+  });
+})();
