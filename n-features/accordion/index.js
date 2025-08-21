ace74c2451bed1ca98bf8a4253decc5ad58fb96f
Nicelx <relush321@gmail.com>
2025-08-21 18:03:31 +0300

    creating simple taxonomy

--- n-features/accordion/index.js
+++ n-features/accordion/index.js
@@ -1,38 +1,44 @@
 // in development and testing Accordion v0
 
 class NAccordion {
-    constructor(selector) {
-        this.container = document.querySelector(selector);
-        if (this.container) {
-            this.items = this.container.querySelectorAll('.n-accordion__item');
+  constructor(selector) {
+    this.container = document.querySelector(selector);
+    if (this.container) {
+      this.items = this.container.querySelectorAll(".n-accordion__item");
 
-            this.items.forEach(item => {
-                item.addEventListener('click', () => {
-                    const panel = item.querySelector('.n-accordion__panel');
+      this.items.forEach((item) => {
+        item.addEventListener("click", () => {
+          const panel = item.querySelector(".n-accordion__panel");
 
-                    if (item.classList.contains('n-accordion__item--opened')) {
-                        panel.style.height = '0px';
-                        item.classList.remove('n-accordion__item--opened');
-                        this.isSomethingOpen = false;
-                        return;
-                    } else {
-                        panel.style.height = `${panel.scrollHeight}px`;
-                        if (this.isSomethingOpen) {
-                            const opened = this.container.querySelector('.n-accordion__item--opened');
-                            const openedPanel = opened.querySelector('.n-accordion__panel')
+          if (item.classList.contains("n-accordion__item--opened")) {
+            panel.style.height = "0px";
+            item.classList.remove("n-accordion__item--opened");
+            this.isSomethingOpen = false;
+            return;
+          } else {
+            panel.style.height = `${panel.scrollHeight}px`;
+            if (this.isSomethingOpen) {
+              const opened = this.container.querySelector(
+                ".n-accordion__item--opened"
+              );
+              const openedPanel = opened.querySelector(".n-accordion__panel");
 
-                            openedPanel.style.height = '0px';
-                            opened.classList.remove('n-accordion__item--opened');
-                        }
-                        item.classList.add('n-accordion__item--opened');
-                        this.isSomethingOpen = true;
-                    }
-                    // item.classList.toggle('n-accordion__item--opened')
-                })
-
-            })
-
-        } else throw new Error('this.container is not defined')
-    }
-    isSomethingOpen = false;
+              openedPanel.style.height = "0px";
+              opened.classList.remove("n-accordion__item--opened");
+            }
+            item.classList.add("n-accordion__item--opened");
+            this.isSomethingOpen = true;
+          }
+          // item.classList.toggle('n-accordion__item--opened')
+        });
+      });
+    } else throw new Error("this.container is not defined");
+  }
+  isSomethingOpen = false;
 }
+
+console.log("hi");
+
+window.addEventListener("DOMContentLoaded", () => {
+  new NAccordion(".acc-container-selector");
+});
