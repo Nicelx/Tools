ad66055ec35a2ad37ccf454f4f19742d24b7e8a4
Nicelx <relush321@gmail.com>
2025-11-06 16:19:00 +0300

    underline-in

--- code-snippets/mysl.sql
+++ code-snippets/mysl.sql
@@ -3,3 +3,6 @@
 CREATE TABLE products_backup AS 
 SELECT * FROM products;
 
+-- add 3 hours to timezone. Date--from timezone--to timezone
+CONVERT_TZ('2025-10-05 21:00:00', '+00:00', '+03:00')
+-- result: '2025-10-06 00:00:00' (+3h)
