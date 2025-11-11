ddb5612ddfaec051160df780ad47ede5d154ccb7
Nicelx <relush321@gmail.com>
2025-11-11 17:49:42 +0300

    demo mouse parallax, add letter split for stagged animation uses

--- expreriments/stagged animation/index.js
+++ expreriments/stagged animation/index.js
@@ -1,8 +1,9 @@
 class NStaggedAnimation {
-    // v.0 developing
+    // v.01 developing
     constructor(cardSelector, options) {
         this.cards = document.querySelectorAll(cardSelector);
         this.init();
+        this.timeGap = options.timeGap;
     }
     init() {
         this.parentEl = this.cards[0].parentElement;
@@ -12,7 +13,7 @@ class NStaggedAnimation {
                 if (entry.isIntersecting) {
                     this.cards.forEach((card, index) => {
                         card.style.animation = 'n-staged-animation 1s ease-out forwards';
-                        card.style.animationDelay = `${(index * 0.08) + 0.3}s`;
+                        card.style.animationDelay = `${(index * this.timeGap) + 0.3}s`;
                     })
                 }
             });
