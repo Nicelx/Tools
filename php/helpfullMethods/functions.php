408c5fb0db8358d6d53db43fcc11ee946c6742d1
Nicelx <relush321@gmail.com>
2026-03-04 18:05:33 +0300

    exists

---
+++ php/helpfullMethods/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+// проверяет, существует ли функция 
+function_exists('function_name')
+
+// можно так
+if (!function_exists('my_fun') {
+    function my_fun() {
+        // определяем функцию
+    }
+})
